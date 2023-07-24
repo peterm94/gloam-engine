f218dc398c0d7bd8a391bb675ca606d4d498d816
Peter <peterm1994@hotmail.com>
2023-07-24 21:24:47 +1000

    text rendering

--- src/draw.rs
+++ src/draw.rs
@@ -1,5 +1,6 @@
 use macroquad::prelude::*;
 use wasm_bindgen::prelude::*;
+use web_sys::console::log_1;
 
 use crate::game::{Gloam, TEXTURES};
 
@@ -40,6 +41,10 @@ impl Gloam {
         draw_rectangle_lines(x, y, w, h, thickness, Gloam::to_color(color));
     }
 
+    pub fn draw_rectangle_filled(x: f32, y: f32, w: f32, h: f32, color: u32) {
+        draw_rectangle(x, y, w, h, Gloam::to_color(color));
+    }
+
     pub fn draw_triangle_filled(x1: f32, y1: f32, x2: f32, y2: f32, x3: f32, y3: f32, color: u32) {
         draw_triangle(Vec2::new(x1, y1), Vec2::new(x2, y2), Vec2::new(x3, y3), Gloam::to_color(color));
     }
@@ -50,8 +55,36 @@ impl Gloam {
 
     // TODO ttf font resource loading
     // TODO draw_text_ex
-    pub fn draw_text(text: &str, x: f32, y: f32, size: f32, color: u32) {
-        draw_text(text, x, y, size, Gloam::to_color(color));
+    pub fn draw_text(text: &str, x: f32, y: f32, size: f32, color: u32, multi_line: Option<bool>) {
+        if let Some(false) = multi_line {
+            draw_text(text, x, y, size, Gloam::to_color(color));
+            return;
+        }
+        let color = Gloam::to_color(color);
+        let line_height = measure_text("Ay", None, size as _, 1.).height;
+        text.split('\n').enumerate().for_each(|(i, line)| {
+            draw_text(line, x, y + (line_height * i as f32), size, color);
+        })
+    }
+
+    pub fn measure_text(text: &str, size: u16, multi_line: Option<bool>) -> TextDims {
+        let dims = measure_text(text, None, size, 1.0);
+        if let Some(false) = multi_line {
+            return dims.into();
+        }
+        let mut width = 0.0;
+        let mut height = measure_text("Ay", None, size as _, 1.).height;
+        let mut offset_y = dims.offset_y;
+
+        // This is a pixel short without this, not sure how it scales to lots of lines.
+        let mut lines = 0.1;
+        text.split('\n').for_each(|line| {
+            let line_dims = measure_text(line, None, size as _, 1.0);
+            width = f32::max(width, line_dims.width);
+            lines += 1.0;
+        });
+
+        TextDims { width, height: height * lines, offset_y }
     }
 
     fn to_color(value: u32) -> Color {
@@ -60,6 +93,19 @@ impl Gloam {
         let blue = value & 0xFF;
 
         color_u8!(red, green, blue, 255)
+    }
+}
+
+#[wasm_bindgen]
+pub struct TextDims {
+    pub width: f32,
+    pub height: f32,
+    pub offset_y: f32,
+}
+
+impl Into<TextDims> for TextDimensions {
+    fn into(self) -> TextDims {
+        TextDims { width: self.width, height: self.height, offset_y: self.offset_y }
     }
 }
 
